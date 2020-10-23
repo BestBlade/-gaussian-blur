21df00e93efb786108a380d48903cfd25b8639ed
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-10-23 17:05:34 +0800

    Update 高斯模糊.cpp

--- 高斯模糊.cpp
+++ 高斯模糊.cpp
@@ -2,8 +2,8 @@
 *┌────────────────────────────────────────────────┐
 *│　描    述：根据原理编写程序，实现高斯模糊													   │
 *│　作    者：牟峙桦|BestBlade																	   │
-*│　版    本：2.0																			   	   │
-*│　创建时间：2020.07.11																		   │
+*│　版    本：3.0																			   	   │
+*│　创建时间：2020.10.23																		   │
 *└────────────────────────────────────────────────┘
 */
 
@@ -13,95 +13,74 @@
 #include"opencv2/imgproc/imgproc.hpp"
 #include <stdio.h>
 #include <math.h>																				/*	exp()函数	*/
-# define pi 3.1414926
+# define pi 3.1415926535898
 
 using namespace std;
 using namespace cv;
 
 
 Mat mygaussianBlur(Mat img, Size temp, float sigma) {
-	if (img.rows < temp.height || img.cols < temp.width) {
-		cerr << "the size of template is not match the original picture" << endl;
+	if ((temp.height % 2) == 0 || (temp.width % 2) == 0) {
+		cerr << "the size of template is not 2M+1*2N+1" << endl;
 	}
-
-	if (temp.height % 2  == 0 || temp.width % 2 == 0) {
-		cerr << "template is not 2N+1 size" << endl;
-	}
-
-	Mat res(img.rows, img.cols, img.type());
-	int row_start = temp.height >> 1;
-	int col_start = temp.width >> 1;
-	float weight = 2 * pi * sigma * sigma;
-
-	//生成高斯模板
-	vector<vector<float> > mygaussianTemplate(temp.height, vector<float>(temp.width));
+	vector<vector<float> >gaussiantemplate(temp.height, vector<float>(temp.width));
 	float wij_sum = 0;
-	for (int i = 0; i < temp.height; i++) {
-		for (int j = 0; j < temp.width; j++) {
-			int wi = i - row_start;
-			int wj = j - col_start;
-			mygaussianTemplate[i][j] = weight * exp(-(wi * wi + wj * wj) / (2 * (sigma * sigma)));
-			wij_sum += mygaussianTemplate[i][j];
+	int cx = temp.height / 2;
+	int cy = temp.width / 2;
+	for (int i = 0; i < gaussiantemplate.size(); i++) {
+		for (int j = 0; j < gaussiantemplate[i].size(); j++) {
+			float wij = exp(-((i - cx) * (i - cx) + (j - cy) * (j - cy)) / (2 * sigma * sigma)) / (2 * pi * sigma * sigma);
+			wij_sum += wij;
+			gaussiantemplate[i][j] = wij;
 		}
 	}
-
-	for (int i = 0; i < temp.height; i++) {
-		for (int j = 0; j < temp.width; j++) {
-			mygaussianTemplate[i][j] /= wij_sum;
+	for (int i = 0; i < gaussiantemplate.size(); i++) {
+		for (int j = 0; j < gaussiantemplate[i].size(); j++) {
+			gaussiantemplate[i][j] /= wij_sum;
 		}
 	}
-	//单通道图像
-	if (res.channels() == 1) {
-		for (int x = row_start; x < res.rows - row_start; x++) {
-			for (int y = col_start; y < res.cols - col_start; y++) {
-				float val = 0;
-
-				for (int i = 0; i < temp.height; i++) {
-					for (int j = 0; j < temp.width; j++) {
-						val += img.at<uchar>(x - row_start + i, y - col_start + j) * mygaussianTemplate[i][j];
-					}
-				}
-
-				res.at<uchar>(x, y) = uchar(val);
-			}
-		}
-	}
-	//三通道图像
-	else {
-		for (int c = 0; c < res.channels(); c++) {
-			for (int x = row_start; x < res.rows - row_start; x++) {
-				for (int y = col_start; y < res.cols - col_start; y++) {
-					float val = 0;
-					for (int i = 0; i < temp.height; i++) {
-						for (int j = 0; j < temp.width; j++) {
-							val += img.at<Vec3b>(x - temp.height / 2 + i, y - temp.width / 2 + j)[c] * mygaussianTemplate[i][j];
-						}
-					}
-
-					res.at<Vec3b>(x, y)[c] = uchar(val);
+	Mat dst(img.size(), img.type());
+	for (int x = cx; x < img.rows - cx; x++) {
+		for (int y = cy; y < img.cols - cy; y++) {
+			float val = 0;
+			for (int i = 0; i < gaussiantemplate.size(); i++) {
+				for (int j = 0; j < gaussiantemplate[i].size(); j++) {
+					val += img.at<uchar>(x - cx + i, y - cy + j) * gaussiantemplate[i][j];
 				}
 			}
+			dst.at<uchar>(x, y) = uchar(val);
+		}
+	}
+	return dst;
+}
+
+Mat minusPic(Mat img1, Mat img2) {
+	Mat res(img1.size(), CV_8UC1);
+	for (int x = 0; x < img1.rows; x++) {
+		for (int y = 0; y < img1.cols; y++) {
+			res.at<uchar>(x, y) = abs(img1.at<uchar>(x, y) - img2.at<uchar>(x, y));
 		}
 	}
 	return res;
 }
-
 int main()
 {
 	Mat img = imread("C://Users//Chrysanthemum//Desktop//1.png",0);
 	Mat res;
 	Size temp(9, 9);
-	double sigma = 1.4;
+	double sigma = 5;
 	
 	res = mygaussianBlur(img,temp,sigma);										/*	自定义高斯模糊函数	*/
 
 	imshow("原图", img);
-	imshow("3x3,sigma = 1,平滑结果图", res);
+	imshow("9*9,sigma = 5,平滑结果图", res);
 
 	Mat result_pic_opencv_blur;
-	GaussianBlur(img, result_pic_opencv_blur, temp, sigma, BORDER_DEFAULT);				/*	OPENCV自带高斯模糊	*/
+	GaussianBlur(img, result_pic_opencv_blur, temp, sigma);						/*	OPENCV自带高斯模糊	*/
 	imshow("OPENCV GaussianBlur()结果图", result_pic_opencv_blur);
 
+	//计算两图片差值
+	Mat cha = minusPic(result_pic_opencv_blur , res);
+	imshow("差值", cha);
 	waitKey(0);
-	//return 0;
 }
